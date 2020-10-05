b3e9c26e18e1973f3f0550060fca4c15c6c94bcd
alice <alice890308@gmail.com>
2020-10-05 21:36:42 +0800

    init

---
+++ pkg/apis/lsalab/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+// +groupName=lsalab.nthu
+
+// Package v1alpha1 is the v1alpha1 version of the API.
+package v1alpha1 // import "github.com/cloud-native-taiwan/controller101/pkg/apis/cloudnative/v1alpha1"
